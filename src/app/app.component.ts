b2a044b0368216f68743bd133355e2733344c374
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-01-05 20:56:53 -0300

    excluindo-arquivos-appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <app-title></app-title>
+  <router-outlet></router-outlet>`
 })
-export class AppComponent {
-  title = 'meu-primeiro-projeto';
+export class AppComponent implements OnInit{
+  constructor(){}
+
+
+  ngOnInit(): void {}
 }
