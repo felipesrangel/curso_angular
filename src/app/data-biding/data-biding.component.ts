8f65d534563a9584a2bbbff84afb19f0e5fcea35
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-01-12 22:39:03 -0300

    aprendendo property binding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -9,6 +9,10 @@ export class DataBidingComponent implements OnInit{
   public nome: string = "Luiz";
   public idade: number = 24;
   
+  public checkedDisabled: boolean = false;
+
+  public imgSrc:string="https://s2.glbimg.com/SkyLTd6VJy8WiUMg5L6EeUwgyMw=/0x0:620x548/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/B/t/limPwzQmSeI4WJO7haZg/2012-08-15-mf1.jpg";
+  public imgTitle:string="Property Binding";
   
   constructor() {};
 
