78cbffd930fe431d4cd846802f52926851fac70c
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-01-12 23:06:36 -0300

    aprendendo sobre event binding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NumberValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-data-biding',
@@ -14,7 +15,19 @@ export class DataBidingComponent implements OnInit{
   public imgSrc:string="https://s2.glbimg.com/SkyLTd6VJy8WiUMg5L6EeUwgyMw=/0x0:620x548/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/B/t/limPwzQmSeI4WJO7haZg/2012-08-15-mf1.jpg";
   public imgTitle:string="Property Binding";
   
+  public position: {x: number, y: number} = {x:0, y: 0};
+
   constructor() {};
 
   ngOnInit(): void {}
+
+  public alertaInfo(valor: MouseEvent){
+    console.log(valor);
+  }
+
+  public mouseMoveTeste(valor: MouseEvent){
+    console.log(valor);
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
