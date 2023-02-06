d083bdc750f863e35a42062c5bdec0b5e86d8f78
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-02-06 18:21:29 -0300

    diretiva ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-diretivas-estruturais></app-diretivas-estruturais>
-  <router-outlet></router-outlet>`
+  <!-- <app-diretivas-estruturais></app-diretivas-estruturais>
+   -->
+  <app-diretivas-atributos></app-diretivas-atributos>
+  <router-outlet></router-outlet>
+  
+  `
 })
 export class AppComponent implements OnInit{
   constructor(){}
